eb86824b84099bb952db010654bf5c3a3da03893
Heri Setyawan <herisetyawan233@gmail.com>
2025-02-01 05:29:08 +0700

    ✨ feat: add loading page and apply to history page

--- app/pages/transactions/history.tsx
+++ app/pages/transactions/history.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CardHistory from '~/components/card-history'
 import historyStatic from '~/data/static/history'
+import LoadingPage from '../loading/loading';
+import { clearTimeout } from 'timers';
 
 export default function HistoryPage() {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const time = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(time);
+  }, [])
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+
   return (
     <div className="max-w-xl mx-auto p-6 bg-white mt-10">
       <h2 className="text-2xl font-semibold mb-6 text-center">History Transaction</h2>
